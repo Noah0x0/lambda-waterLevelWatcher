b92b666c71c692f33974404874ba1393c1a4b78c
mafuyuk <mafuyuk.m@gmail.com>
2017-08-05 23:06:13 +0900

    add putWaterLevel

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 'use strict';
 
-const Promise = require('bluebird');
+const promise = require('bluebird');
 const request = require('request-promise');
+const aws = require('aws-sdk');
+const s3 = new aws.S3({ apiVersion: '2006-03-01' });
 
 module.exports.handler = (event, context, callback) => {
-    return Promise.coroutine(processEvent)(event, context, callback);
+    return promise.coroutine(processEvent)(event, context, callback);
 }
 
 function *processEvent(event, context, callback) {
@@ -26,5 +28,25 @@ function *processEvent(event, context, callback) {
     // TODO: データの保存をわかりやすくするために浅野川（諸江）に絞っています
     // データ構造が決まったら、絞り込みは解除してループで全川のデータを入れてください
     console.log(data['4357_4_84276000_51']);
-    callback(null, "success");
-};
+    const waterLevel = data['4357_4_84276000_51'];
+
+    putWaterLevel(waterLevel)
+        .then((res) => {
+            console.log(res);
+            callback(null, 'success');
+        })
+        .catch((err) => {
+            console.err(err.stack);
+            callback(err);
+        });
+}
+
+function putWaterLevel(body) {
+    const params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: 'waterLevel.json',
+        Body: JSON.stringify(body),
+        ContentType: 'application/json'
+    };
+    return s3.putObject(params).promise();
+}
