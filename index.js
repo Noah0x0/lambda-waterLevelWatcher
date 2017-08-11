4231d152d9a54fd7812b121aae507ae689150810
ihara-kenta <mayonnaise.3048.meter@gmail.com>
2017-08-11 23:37:36 +0900

    Create convertLocalData func.

--- index.js
+++ index.js
@@ -24,11 +24,9 @@ function *processEvent(event, context, callback) {
         console.error(err.stack);
         callback('end fail');
     });
-    // to quit finally
-    // TODO: データの保存をわかりやすくするために浅野川（諸江）に絞っています
-    // データ構造が決まったら、絞り込みは解除してループで全川のデータを入れてください
-    console.log(data['4357_4_84276000_51']);
-    const waterLevel = data['4357_4_84276000_51'];
+
+    const waterLevel = convertLocalData(data);
+    console.log(waterLevel);
 
     putWaterLevel(waterLevel)
         .then((res) => {
@@ -49,4 +47,27 @@ function putWaterLevel(body) {
         ContentType: 'application/json'
     };
     return s3.putObject(params).promise();
+}
+
+// 石川河川水位APIに合わせて、値を変換する
+function convertLocalData(data , locale) {
+    // TODO: localeで切り替えられるようにする
+    // データの保存をわかりやすくするために浅野川（諸江）に絞っています
+    const asano = '4_10';
+    const timeLine = data['4357_4_84276000_51']['timeLineData'];
+    const town = data['4357_4_84276000_51']['masterData']['townName'];
+    const point = data['4357_4_84276000_51']['masterData']['pointNameShort'];
+
+    // TOOD レスポンスの雛形オブジェクトの作成
+    const waterLevel = {
+        riverName: data['4357_4_84276000_51']['masterData']['riverName'],
+        height:  data['4357_4_84276000_51']['customData']['stageAlarmLv7'],
+        timestamp:  data['4357_4_84276000_51']['customData']['stageObsTime'],
+        waterLevel:  timeLine[timeLine.length - 1][asano]['dataStr'],
+        dataTrend:  timeLine[timeLine.length - 1][asano]['dataTrend'],
+        dataLevel: timeLine[timeLine.length - 1][asano]['dataLevel'],
+        observatory:  town + point
+    };
+
+    return waterLevel;
 }
