b5bef48fa9c012e0b88deeb53858e70f39c0f6bc
ihara-kenta <mayonnaise.3048.meter@gmail.com>
2017-08-12 19:11:04 +0900

    Replace some variables to constants.

--- modules/convert.js
+++ modules/convert.js
@@ -5,19 +5,25 @@ module.exports = {
         // TODO localeで切り替えられるようにする
 
         // 石川河川水位APIに合わせて、値を変換する
-        const asano = '4_10';
-        const timeLine = data['4357_4_84276000_51']['timeLineData'];
-        const town = data['4357_4_84276000_51']['masterData']['townName'];
-        const point = data['4357_4_84276000_51']['masterData']['pointNameShort'];
+        const riverNum = '4_10'; // asano river
+        const observatoryNum = '4357_4_84276000_51'; // okihashi observatory
+
+        const timeLine = data[observatoryNum]['timeLineData'];
+        const latestData = timeLine[timeLine.length - 1];
+        const masterData = data[observatoryNum]['masterData'];
+        const customData = data[observatoryNum]['customData'];
+
+        const town = masterData['townName'];
+        const point = masterData['pointNameShort'];
 
         // TODO レスポンスの雛形オブジェクトの作成
         return {
-            riverName: data['4357_4_84276000_51']['masterData']['riverName'].trim(),
-            height:  data['4357_4_84276000_51']['customData']['stageAlarmLv7'],
-            timestamp:  data['4357_4_84276000_51']['customData']['stageObsTime'],
-            waterLevel:  timeLine[timeLine.length - 1][asano]['dataStr'],
-            dataTrend:  timeLine[timeLine.length - 1][asano]['dataTrend'],
-            dataLevel: timeLine[timeLine.length - 1][asano]['dataLevel'],
+            riverName: masterData['riverName'].trim(),
+            height:  customData['stageAlarmLv7'],
+            timestamp:  customData['stageObsTime'],
+            waterLevel:  latestData[riverNum]['dataStr'],
+            dataTrend:  latestData[riverNum]['dataTrend'],
+            dataLevel: latestData[riverNum]['dataLevel'],
             observatory:  town + point
         };
     },
