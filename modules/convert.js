55c7e8e36d46dcd4dbb1b2b046e3c03c2397fad1
ihara-kenta <mayonnaise.3048.meter@gmail.com>
2017-08-12 23:41:28 +0900

    Refactor timestamp to follow ISO8601.

--- modules/convert.js
+++ modules/convert.js
@@ -1,10 +1,13 @@
 'use strict';
+const moment = require('moment');
 
 module.exports = {
     toCommonJson:  (data , locale = null) => {
         // TODO localeで切り替えられるようにする
 
         // 石川河川水位APIに合わせて、値を変換する
+        // constants
+        const ISO8601 = "YYYY-MM-DD HH:mm:ss";
         const riverNum = '4_10'; // asano river
         const observatoryNum = '4357_4_84276000_51'; // okihashi observatory
 
@@ -16,11 +19,16 @@ module.exports = {
         const town = masterData['townName'];
         const point = masterData['pointNameShort'];
 
+        // creating timestamp
+        const dateSplit = masterData['etim'].split('-');
+        const timeStr = dateSplit[0] + '-' +dateSplit[1] + '-' + dateSplit[2] + " " + dateSplit[3] + ":" + dateSplit[4];
+        const timestamp = new moment(timeStr);
+
         // TODO レスポンスの雛形オブジェクトの作成
         return {
             riverName: masterData['riverName'].trim(),
             height:  customData['stageAlarmLv7'],
-            timestamp:  customData['stageObsTime'],
+            timestamp:  timestamp.format(ISO8601),
             waterLevel:  latestData[riverNum]['dataStr'],
             dataTrend:  latestData[riverNum]['dataTrend'],
             dataLevel: latestData[riverNum]['dataLevel'],
